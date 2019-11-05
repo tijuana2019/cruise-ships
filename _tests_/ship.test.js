6f161f777c9e50534dd41babe2b6dfcf5f94812a
Jo <jo_serendipity@icloud.com>
2019-11-05 20:01:19 +0000

    created test and code to check if a ship is sailing

--- _tests_/ship.test.js
+++ _tests_/ship.test.js
@@ -8,5 +8,18 @@ describe("ship", () => {
     it("has a starting port", () => {
         const ship = new Ship("Dover");
         expect(ship.startingPort).toBe("Dover");
+    });
+    it("enables the ship to set sail from a port", () => {
+        const ship = new Ship("Dover", 1);
+        ship.setSail();
+        expect(ship.sailing).toBe(true);
+
+        const ship1 = new Ship("Dover", 0);
+        ship1.setSail();
+        expect(ship1.sailing).toBe(false);
+
+        const ship2 = new Ship("Dover", 7);
+        ship2.setSail();
+        expect(ship2.sailing).toBe(true);
     })
     })
--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,20 @@
 class Ship {
-    constructor(startingPort) {
+    constructor(startingPort, passengers) {
         this.startingPort = startingPort;
+        this.passengers = passengers;
+        this.sailing = true;
     }
+
+    setSail() {
+        if(this.passengers >= 1) {
+            this.sailing = true;
+        } else {
+            this.sailing = false;
+        }
+
+        
+      }
+   
 }
 
 
